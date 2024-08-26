da43bdc019f39c521e8076e25529c7f0799de49c
Cleiton Gama <cleitonmendesgama@gmail.com>
2024-08-26 16:48:49 -0300

    Integração do formulário com a tabela no banco de dados

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\produto;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/cadastro_prod',function (Request $dados) {
+    produto::create([
+        'nome' => $dados-> nome,
+        'category' => $dados-> category
+    ]);
+    echo "Cadastro realizado!";
 });
